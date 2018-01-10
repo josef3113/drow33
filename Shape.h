c800203ee446813f3fb0f0ce4b2a1dc135ff0c51
josef3113 <33261454+josef3113@users.noreply.github.com>
2018-01-10 22:19:10 +0200

    final ver 1.0

--- Shape.h
+++ Shape.h
@@ -7,7 +7,6 @@
 #include "afx.h"
 #include "afxdialogex.h"
   
-
 #include <iostream>
 using namespace std;
  */ 
@@ -16,7 +15,7 @@ class MShape: public CObject
 {
 	
 	DECLARE_SERIAL (MShape)
-public:
+protected:
 	
 	 int R,G,B;
 	// CPoint A;
@@ -60,6 +59,24 @@ public:
 			//!!7 //ar >> KIND;
 		}
 	}
+	
+	virtual int getR() {return R;}
+	virtual int getG() { return G; }
+	virtual int getB() { return B; }
+	virtual int getAx() { return this->Ax; }
+	virtual int getAy() { return this->Ay; }
+
+	virtual int getCx() { return this->Cx; }
+	virtual int getCy() { return this->Cy; }
+
+	virtual void setR(int newR) { this->R = newR; }
+	virtual void setG(int newG) { this->G = newG;; }
+	virtual void setB(int newB) { this->B = newB; }
+
+	virtual void setAx(int newAx){ this->Ax = newAx;}
+	virtual void setAy(int newAy) { this->Ay = newAy; }
+	virtual void setCx(int newCx) { this->Cx = newCx; }
+	virtual void setCy(int newCy) { this->Cy = newCy; }
 	
 };
 
